5b527f75284e48d1ecc5c9fd35bdb2e2204b4ceb
Giang Dao <giang.dao@successsoftware.global>
2021-10-24 00:27:13 +0700

    Revert webpack favicon & build

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -43,6 +43,7 @@ module.exports = {
     // Generates deprecation warning: https://github.com/jantimon/html-webpack-plugin/issues/1501
     new HtmlWebpackPlugin({
       title: 'Fire Shooter - Phaser 3',
+      favicon: paths.src + '/images/favicon.png',
       template: paths.src + '/template.html', // template file
       filename: 'index.html', // output file
     }),
