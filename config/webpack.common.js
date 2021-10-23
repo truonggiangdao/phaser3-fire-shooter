8fe6a720ff28ad03114aa03e2a7159e77072696a
Giang Dao <giang.dao@successsoftware.global>
2021-10-24 00:24:20 +0700

    Update webpack

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -42,8 +42,7 @@ module.exports = {
     // Generates an HTML file from a template
     // Generates deprecation warning: https://github.com/jantimon/html-webpack-plugin/issues/1501
     new HtmlWebpackPlugin({
-      title: 'webpack Boilerplate',
-      favicon: paths.src + '/images/favicon.png',
+      title: 'Fire Shooter - Phaser 3',
       template: paths.src + '/template.html', // template file
       filename: 'index.html', // output file
     }),
